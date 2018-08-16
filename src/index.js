ca7d837faece41f239f21bc5cbafe4667cd2a216
sibZhang <940066231@qq.com>
2018-08-16 16:40:40 +0800

    v0.0.1 初次提交

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("xx-cache");
+class  FastCache {
+    constructor() {
+        this.list = {}
+    }
+    set(key, value) {
+        this.list[key] = value
+    }
+    get(key) {
+        return this.list[key]
+    }
+}
+
+window.FastCache = FastCache
